6f963a665e028939fb76c06fda2f66c6e07a3184
jiangzhengdu <52695758+jiangzhengdu@users.noreply.github.com>
2020-06-19 07:38:59 +0800

    Update Parallel_Boardcast_MPI.c

--- Parallel_Boardcast_MPI.c
+++ Parallel_Boardcast_MPI.c
@@ -1,17 +1,20 @@
 /*
-这个程序是每个进程单独搜一个树枝，然后得到自己本树枝的最好回路，然后一起发送给
-process 0,process 0进行归约判断，看哪个的cost最小，就说明哪个是全局最佳
+这个程序是每个进程单独搜一个树枝，然后得到自己一个最好回路，然后发送给
+所有的process，每个process都需要去看看是否这是最好的cost
+这个是基于那个归约的程序改的
 
 
-不足之处：mpi传递不合适，如果改成传递Tour对象会好点，可是tm的老师ppt上没有吧？醉了，这网课
-真的无聊，不就是调用各种api吗？无聊无聊
-8月份考试咋办，唉
+不足之处：mpi传递不合适 
+不能使用boardcast？很诡异，按照ppt的办法来了
+
+！！！题目要求只传递cost？那路径还保存吗？我直接不保存，直接输出cost了，别怨我hhh
 */
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
 #include<mpi.h>
-int node_num=7;     //city number
+int node_num=5;     //city number
+int NEW_COST_TAG =1000;
 typedef struct 
 {
     int pre;
@@ -122,21 +125,21 @@ int City_count(Tour curr_tour){
     }
     return num;
 }
-int Best_tour(Tour curr_tour, Tour best_tour){    //if the current tour is the best
+// int Best_tour(Tour curr_tour, Tour best_tour){    //if the current tour is the best
 
-    if(curr_tour.total_cost<best_tour.total_cost)
-    return 1;
-    else return 0;
-}
+//     if(curr_tour.total_cost<best_tour.total_cost)
+//     return 1;
+//     else return 0;
+// }
 
-void  Update_best_tour(Tour curr_tour, Tour * best_tour){    
+// void  Update_best_tour(Tour curr_tour, Tour * best_tour){    
 
    
-    best_tour->total_cost=curr_tour.total_cost;
-    int i=0;
-    for(i=0;i<node_num;i++)
-    best_tour->route[i]=curr_tour.route[i];
-}
+//     best_tour->total_cost=curr_tour.total_cost;
+//     int i=0;
+//     for(i=0;i<node_num;i++)
+//     best_tour->route[i]=curr_tour.route[i];
+// }
 void Add_city(Tour * curr_tour,int nbr,MAP_NODE map[]){
     int i=0,j=0;
      for(i=0;;i++)
@@ -170,11 +173,15 @@ void Remove_last_city(Tour * curr_tour){
 int main( int argc, char *argv[] )
 {
     Tour best_tour;
-    best_tour.total_cost=9999999;
+   // best_tour.total_cost=9999999;
     MAP_NODE map[1000];
     Init_map(map);
     int i=node_num*(node_num-1),j=0,k=0;
     int nbr=0;
+    int received_cost=0;
+    int best_tour_cost=99999;
+    int msg_avail=0;
+    int dest=0;
     STACK_Route stack;
     Init_stack(&stack);
     Tour curr_tour,tour;
@@ -184,9 +191,8 @@ int main( int argc, char *argv[] )
     MPI_Status status;
     MPI_Comm_rank( MPI_COMM_WORLD, &rank );
     MPI_Comm_size( MPI_COMM_WORLD, &size );
-    if(rank!=0){
     tour.route[0]=0;
-    tour.route[1]=rank;
+    tour.route[1]=rank+1;
     for(k=2;k<node_num;k++)
     tour.route[k]=-1;
     Push(&stack,tour);
@@ -194,8 +200,20 @@ int main( int argc, char *argv[] )
         curr_tour=Pop(&stack);
         if(City_count(curr_tour)==node_num)
         {
-            if(Best_tour(curr_tour,best_tour))
-            Update_best_tour(curr_tour,&best_tour);
+            if(curr_tour.total_cost<best_tour_cost)
+            best_tour_cost=curr_tour.total_cost;
+            for(dest=0;dest<size;dest++){
+            if(dest!=rank)
+            MPI_Send(&best_tour_cost,1,MPI_INT,dest,NEW_COST_TAG, MPI_COMM_WORLD); 
+            }          
+            MPI_Iprobe(MPI_ANY_SOURCE,NEW_COST_TAG,MPI_COMM_WORLD,&msg_avail,&status);
+            while(msg_avail){
+                  MPI_Recv( &received_cost, 1, MPI_INT, status.MPI_SOURCE, NEW_COST_TAG,MPI_COMM_WORLD,MPI_STATUS_IGNORE );
+                  if(received_cost<best_tour_cost)
+                  best_tour_cost=received_cost;
+                  MPI_Iprobe(MPI_ANY_SOURCE,NEW_COST_TAG,MPI_COMM_WORLD,&msg_avail,&status);
+            //printf("%d from %d  and %d \n",rank,size,curr_tour.total_cost);
+            }        
         }else{
                 for(nbr=node_num-1;nbr>=1;nbr--)
                     if(Feasible(curr_tour,nbr,map))
@@ -206,35 +224,32 @@ int main( int argc, char *argv[] )
                     }
         }
      }
-   
+    // int fuck=0;
+    // printf("the best local tour is 0");
+    // for(fuck=1;fuck<node_num;fuck++)
+    // printf("->%d",best_tour.route[fuck]);
+    // printf(" and the best cost is %d from process %d \n",best_tour.total_cost,rank);
+    // int  local_cost=best_tour.total_cost;
+    // MPI_Send( &local_cost, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
 
-   
-    int fuck=0;
-    printf("the best local tour is 0");
-    for(fuck=1;fuck<node_num;fuck++)
-    printf("->%d",best_tour.route[fuck]);
-    printf(" and the best cost is %d from process %d \n",best_tour.total_cost,rank);
-    int  local_cost=best_tour.total_cost;
-    MPI_Send( &local_cost, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-    }
     if(rank==0)
     {
         //Tour alltout[node_num];
-        int all_cost[node_num];
-        int p=0,q=0,process=1;
-        for(p=1;p<node_num;p++)
-        MPI_Recv( &all_cost[p], 1, MPI_INT, p, 0, MPI_COMM_WORLD, &status );
-        int min=all_cost[1];
-        for(q=2;q<=p;q++)
-        {
-            if(all_cost[q]<min)
-            {
-            min=all_cost[q];
-            process=q;
-            }
-        }    
-        printf("\n!!!obviously the best cost is %d from process %d\n",min,process);
-    
+        // int all_cost[node_num];
+        // int p=0,q=0,process=1;
+        // for(p=1;p<node_num;p++)
+        // MPI_Recv( &all_cost[p], 1, MPI_INT, p, 0, MPI_COMM_WORLD, &status );
+        // int min=all_cost[1];
+        // for(q=2;q<=p;q++)
+        // {
+        //     if(all_cost[q]<min)
+        //     {
+        //     min=all_cost[q];
+        //     process=q;
+        //     }
+        // }    
+        // printf("\n!!!obviously the best cost is %d from process %d\n",min,process);
+    printf("Trust me ! the best cost is %d ,please don't ask me the route because i didn't save it and didn't pass it hhh",best_tour_cost);
     
     
     }
